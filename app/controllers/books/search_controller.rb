293dd95c2063a2e6526dfc4e7703dc8d736213c2
okano0120 <okano0120@gmail.com>
2020-06-29 08:42:58 +0000

    Google Books API 日本語指定

--- app/controllers/books/search_controller.rb
+++ app/controllers/books/search_controller.rb
@@ -5,7 +5,7 @@ class Books::SearchController < ApplicationController
     @books = []
     @search_word = params.dig(:q, :keyword)
     if @search_word.present?
-      url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{params[:q][:keyword]}&maxResults=10&startIndex=0")
+      url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{params[:q][:keyword]}&maxResults=10&startIndex=0&langRestrict=ja")
       user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
       f = OpenURI.open_uri(url, { "User-Agent" => user_agent })
     end
