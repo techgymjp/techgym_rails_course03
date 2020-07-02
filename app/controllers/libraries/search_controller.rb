6d831854a7256863ecd4ef5b7ca09519f5ab3a1f
okano0120 <okano0120@gmail.com>
2020-07-02 15:28:05 +0000

    図書館情報取得

--- app/controllers/libraries/search_controller.rb
+++ app/controllers/libraries/search_controller.rb
@@ -2,8 +2,17 @@ require 'open-uri'
 
 class Libraries::SearchController < ApplicationController
   def new
-  end
+    @city = params.dig(:q, :city)
+    if @city.present?
+      url = URI.encode("http://api.calil.jp/library?appkey=#{Rails.application.credentials.calil_app_key}&city=#{@city}&format=json&callback= ")
+      user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+      f = OpenURI.open_uri(url, { "User-Agent" => user_agent })
+      libraries_json = JSON.load(f.read)
 
+      render plain: libraries_json
+    end
+  end
+ 
   def create
     render plain: params
   end
