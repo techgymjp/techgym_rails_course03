cba9baa43836fb42cca3f2ec24389552a285ab8c
techgym <tadashifujinaga@gmail.com>
2024-07-22 20:26:36 +0900

    Update search_controller.rb
    pref=東京都を追加。APIの仕様変更につき。

--- app/controllers/libraries/search_controller.rb
+++ app/controllers/libraries/search_controller.rb
@@ -4,7 +4,7 @@ class Libraries::SearchController < ApplicationController
   def new
     @city = params.dig(:q, :city)
     if @city.present?
-      url = URI.encode("http://api.calil.jp/library?appkey=#{Rails.application.credentials.calil_app_key}&city=#{@city}&format=json&callback= ")
+      url = URI.encode(“https://api.calil.jp/library?appkey=#{Rails.application.credentials.calil_app_key}&pref=東京都&city=#{@city}&format=json&callback=“)
       user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
       f = OpenURI.open_uri(url, { "User-Agent" => user_agent })
       libraries_json = JSON.load(f.read)
